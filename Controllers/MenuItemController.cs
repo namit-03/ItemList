76406610edbd00a811c1957a183e81f113cdc527
unknown <pundirnamit0103@gmail.com>
2020-07-25 09:29:42 +0530

    initiall commit

---
+++ Controllers/MenuItemController.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MenuItemListingWebApi.Models;
+
+namespace MenuItemListingWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MenuItemController : ControllerBase
+    {
+        
+       public List<MenuItem> item = new List<MenuItem>(){
+            new MenuItem
+            {
+            Id =1,
+            freeDelivery=true,
+            Name="Burger",
+            Price=50.0,
+            dateofLaunch=Convert.ToDateTime("07/05/2020"),
+            Active=true
+            },
+          new MenuItem
+            {
+                Id =2,
+            freeDelivery=true,
+            Name="CheeseCake",
+            Price=120.0,
+            dateofLaunch=Convert.ToDateTime("07/12/2020"),
+            Active=true
+            },
+          new MenuItem
+            {
+                Id =3,
+            freeDelivery=true,
+            Name="PlainPizza",
+            Price=90.0,
+            dateofLaunch=Convert.ToDateTime("07/05/2020"),
+            Active=true
+            },
+           new MenuItem
+            {
+                Id =4,
+            freeDelivery=true,
+            Name="Icecream",
+            Price=90.0,
+            dateofLaunch=Convert.ToDateTime("07/05/2020"),
+            Active=true
+            }
+};
+
+      // GET: api/MenuItem
+
+      [HttpGet]
+        public  List<MenuItem> Get()
+        {
+            
+            return item;
+
+    }
+
+        // GET: api/MenuItem/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            string name = "";
+            var check = item.FirstOrDefault((p) => p.Id == id);
+            name = check.Name;
+            return name;
+        }
+
+        // POST: api/MenuItem
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/MenuItem/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Controllers/TokenController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using MenuItemListingWebApi.Models;
+using Microsoft.Extensions.Configuration;
+using MenuItemListingWebApi.Controllers;
+
+namespace MenuItemListingWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TokenController : ControllerBase
+    {
+
+            private IConfiguration _config;
+
+            public TokenController(IConfiguration config)
+            {
+                _config = config;
+            }
+
+            [HttpPost]
+            public IActionResult Login([FromBody] AuthenticateUser login)
+            {
+                IActionResult response = Unauthorized();
+                var user = AuthenticateUser(login);
+
+                if (user != null)
+                {
+                    var tokenString = GenerateJSONWebToken(user);
+                    response = Ok(new { token = tokenString });
+                }
+
+                return response;
+            }
+
+            private string GenerateJSONWebToken(AuthenticateUser userInfo)
+            {
+                var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+                var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+                var token = new JwtSecurityToken(_config["Jwt:Issuer"],
+                  _config["Jwt:Issuer"],
+                  null,
+                  expires: DateTime.Now.AddMinutes(120),
+                  signingCredentials: credentials);
+
+                return new JwtSecurityTokenHandler().WriteToken(token);
+            }
+
+            private AuthenticateUser AuthenticateUser(AuthenticateUser login)
+            {
+                AuthenticateUser user = null;
+
+                //Validate the User Credentials    
+                //Demo Purpose, I have Passed HardCoded User Information    
+                if (login.Name == "Namit")
+                {
+                    user = new AuthenticateUser { Name = "Namit", Password = "Namit123*@" };
+                }
+                return user;
+            }
+        }
+}
